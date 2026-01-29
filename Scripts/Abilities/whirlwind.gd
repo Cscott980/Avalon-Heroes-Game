63658b4187b1ffa3a95d54d16ca0fbcd4e3922ef
Cscott980 <chrisscottatoz@gmail.com>
2026-01-29 23:00:15 +0900

    whirlwind ability implementation

---
+++ Scripts/Abilities/whirlwind.gd
@@ -0,0 +1,10 @@
+class_name Whirlwind extends Node3D
+
+
+
+func _on_hit_box_body_entered(body: Node3D) -> void:
+	pass # Replace with function body.
+
+
+func _on_dps_timeout() -> void:
+	pass # Replace with function body.
