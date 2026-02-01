067cff1810ec052e47bc477389fe83a6dfe96189
Cscott980 <chrisscottatoz@gmail.com>
2026-02-02 00:10:47 +0900

    added Hoarder character assets and related resources working on hoarder character model and textures random stat generation for leveling system

--- Scripts/Components/Entity/PregressionComponent.gd
+++ Scripts/Components/Entity/PregressionComponent.gd
@@ -4,6 +4,28 @@ signal level(current_level:int)
 signal stat_selected(stat: int, amount: int)
 signal exp_collected(amount: int)
 
-#@export var exp_collector: CollectorComponent
+#@export var exp_collector: DropCollectorComponent
+#var random_stat: RandomStatResource
 
-var 
+var xp_amount: int = 0
+
+func _ready() -> void:
+	pass
+
+func level_up(current_level: int) -> void:
+	var new_level = current_level + 1
+	#await random_stat_selector()
+	emit_signal("level", new_level)
+
+func random_stat_generator() -> Dictionary:
+	var stat: Dictionary = {}
+	var stat_generated: String = StatConst.STAT_REFERENCE[randi_range(1,5)]
+	var random_value: float = randf_range(0.05, 0.20)
+	stat[stat_generated] = random_value
+	return stat
+
+func random_stat_selector() -> Dictionary:
+	var selected_stat: Dictionary = {}
+	return {}
+	
+	
