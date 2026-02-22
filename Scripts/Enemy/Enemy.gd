655292c025d1bc89aebd52efb2ee9661d7754bd2
Cscott980 <chrisscottatoz@gmail.com>
2026-02-22 16:06:07 +0900

    change: Update Enemy Animations and Combat Mechanics

--- Scripts/Enemy/Enemy.gd
+++ Scripts/Enemy/Enemy.gd
@@ -27,14 +27,14 @@ func enemy_init() -> void:
 	state_machine.Initializer()
 	if enemy_data == null or not is_instance_valid(enemy_data):
 		return
-	ai_movement_component.apply_movement_data(enemy_data.movement_data, enemy_data.weapon_data)
 	enemy_visuals_component.apply_visuals(enemy_data.enemy_mesh)
 	enemy_main_hand_component.apply_mainhand_weapon_visual_data(enemy_data.weapon_data)
 	weapon_shield_relic.apply_offhand_weapon_visual_data(enemy_data.weapon_data)
+	enemy_animation_component.apply_animation_playback(enemy_data)
+	ai_movement_component.apply_movement_data(enemy_data.movement_data, enemy_data.weapon_data)
 	enemy_melee_combat_component.apply_melee_weapon_data(enemy_data.weapon_data)
 	enemy_world_data_display.apply_world_display_data(enemy_data.name, enemy_level_component.level, enemy_data.max_health)
 	enemy_health_component.apply_health_data(enemy_data.max_health, enemy_data.stats)
-	enemy_animation_component.apply_animation_playback(enemy_data)
 	
 func _on_ai_controller_component_target_in_attack_dist(status: bool) -> void:
 	if enemy_health_component.is_dead: 
@@ -54,14 +54,12 @@ func _on_hurt_box_component_hit() -> void:
 		return
 	if state_machine:
 		state_machine.change_state("HurtState")
-
-
+	
 func _on_ai_controller_component_wandering() -> void:
 	if enemy_health_component.is_dead:
 		return
 	if state_machine:
 		state_machine.change_state("WanderState")
-
 
 func _on_ai_controller_component_idling() -> void:
 	if enemy_health_component.is_dead:
